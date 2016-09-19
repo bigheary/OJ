efb637d5d6b7e8487d63fd4e29274f4f822bd201
bigheary <xie.xd@foxmail.com>
2016-09-19 10:20:11 +0800

    9-20

--- src/com/xxd/didi/Main.java
+++ src/com/xxd/didi/Main.java
@@ -3,6 +3,7 @@ package com.xxd.didi;
 /**
  * Created by Administrator on 2016/9/18 0018.
  */
+import java.util.ListIterator;
 import java.util.Scanner;
 import java.util.LinkedList;
 public class Main{
@@ -20,26 +21,14 @@ public class Main{
         }
         LinkedList<Pos> stack = new LinkedList<Pos>();
         visited[0][0] = true;
+        stack.push(new Pos(0, 0));
         boolean res = dfs(data, visited, 0, 0, stack, P);
-        for(int i = 0; i < stack.size()-1; i++){
-            Pos tmp = stack.get(i);
-            System.out.print(tmp+",");
+        while(stack.size() > 1){
+            System.out.print(stack.pollLast()+",");
         }
-        System.out.println(stack.get(stack.size()-1));
+        System.out.println(stack.pollLast());
     }
 
-    /*    private List<Pos> getPath(int[][] data, int P){
-            int n = data.length;
-            int m = data[0].length;
-            int[][] rem = new int[n][m];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    rem[i][j] = Integer.MIN_VALUE;
-                }
-            }
-            rem[0][0] = P;
-
-        }*/
     private static boolean dfs(int[][] data, boolean[][] visited, int x, int y, LinkedList<Pos> stack, int P){
         int n = data.length;
         int m = data[0].length;
@@ -53,7 +42,7 @@ public class Main{
         }
         if(y+1 < m && data[x][y+1]==1 && !visited[x][y+1]){//right
             visited[x][y+1] = true;
-            stack.push(new Pos(x, y));
+            stack.push(new Pos(x, y+1));
             if(dfs(data, visited, x, y+1, stack, P-1)){
                 return true;
             }else{
@@ -64,7 +53,7 @@ public class Main{
 
         if(x-1 >= 0 && data[x-1][y]==1 && !visited[x-1][y]){//up
             visited[x-1][y] = true;
-            stack.push(new Pos(x, y));
+            stack.push(new Pos(x-1, y));
             if(dfs(data, visited, x-1, y, stack, P-3)){
                 return true;
             }else{
@@ -75,7 +64,7 @@ public class Main{
 
         if(x+1 < n && data[x+1][y]==1 && !visited[x+1][y]){//down
             visited[x+1][y] = true;
-            stack.push(new Pos(x, y));
+            stack.push(new Pos(x+1, y));
             if(dfs(data, visited, x+1, y, stack, P)){
                 return true;
             }else{
@@ -85,7 +74,7 @@ public class Main{
         }
         if(y-1 >= 0 && data[x][y-1]==1 && !visited[x][y-1]){//left
             visited[x][y-1] = true;
-            stack.push(new Pos(x, y));
+            stack.push(new Pos(x, y-1));
             if(dfs(data, visited, x, y-1, stack, P-1)){
                 return true;
             }else{
